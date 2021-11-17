e7bda1a4221b81f8d4a546ce2f7f613f6a884e92
Alex Rodríguez <rodriguezsm2013@licifug.ugto.mx>
2021-11-16 18:46:44 -0600

    update app.py, request features from browser

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
 from flask import Flask
-import pandas as pd
+from flask import request
 from model import ideal_houses
+import pandas as pd
 
 
 app = Flask(__name__)
 
 
-@app.route("/ideal_house", methods=['GET'])
+@app.route("/ideal_house", methods=['GET', 'POST'])
 def ideal_house():
+	# take requests from browser
+	surface = int(request.args.get('surface'))
+	bathrooms = int(request.args.get('bathrooms'))
+	bedrooms = int(request.args.get('bedrooms'))
+	parking = int(request.args.get('parking'))
+	price = int(request.args.get('price'))
+	scrap = str(request.args.get('scrap'))
+	# Option to web scraping
+	if scrap == "true":
+		scrap_houses() 
+	
+	# read data
 	df = pd.read_csv('data_houses.csv')
+	# feed the model function 
+	houses = ideal_houses(surface, bathrooms, bedrooms, parking, price)
+	# compile results
 	options = {}
 	for i in range(5):
 		options[i+1] = {"Score": houses[i][0], "URL": df['URl'][houses[i][1]]}
@@ -16,9 +32,8 @@ def ideal_house():
 	return options
 
 
-if __name__ == '__main__':	
-    #scrap_houses()    
-    houses = ideal_houses(1,1,1,1,1)
+if __name__ == '__main__':
+
     app.run(debug=True, port=8080)
 
 
